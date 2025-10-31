3629915db0a0f947358d19fedf1d911e097e464e
amarcmd <amarbader39@gmail.com>
2025-10-31 20:25:53 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let menuToggle = document.getElementById("menuToggle");
+let header=document.querySelector("header");
+let gallery=document.querySelector(".gallery");
+
+
+menuToggle.addEventListener("click",()=>{
+    header.classList.toggle("header-nav-open");
+});
+
+
+
+
