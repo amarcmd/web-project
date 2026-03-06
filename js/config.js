71fb17eb1b347a55a400a1ce1f348cefc827338b
tasneem-AA <AbdAlkareemTT@students.rhu.edu.lb>
2026-03-05 19:04:46 -0800

    jg

--- js/config.js
+++ js/config.js
@@ -1,15 +1,8 @@
 
-function getBasePath() {
-   const path = window.location.pathname;
-    const match = path.match(/^\/[^/]+\//); 
-    return match ? match[0] : '/';
-}
-
 function getProfileUrl() {
-    const base = getBasePath();
     if (window.location.pathname.includes('/pages/')) {
-        return base + 'pages/Profile.html';
+        return 'Profile.html';          
     } else {
-        return base + 'pages/Profile.html';
+        return 'pages/Profile.html';    
     }
 }
